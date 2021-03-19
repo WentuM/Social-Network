fa94a9d3e17d4b8b86de77173456fec15d966815
WentuM <kesand@mail.ru>
2021-03-19 22:29:54 +0300

    as

--- src/main/java/ru/itis/springlectionsdemo/services/SignUpServiceImpl.java
+++ src/main/java/ru/itis/springlectionsdemo/services/SignUpServiceImpl.java
@@ -1,23 +1,23 @@
-package ru.itis.springlectionsdemo.services;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import ru.itis.springlectionsdemo.dto.UserForm;
-import ru.itis.springlectionsdemo.models.User;
-import ru.itis.springlectionsdemo.repositories.UsersRepository;
-
-@Component
-public class SignUpServiceImpl implements SignUpService {
-
-    @Autowired
-    private UsersRepository usersRepository;
-    //
-
-    @Override
-    public void signUp(UserForm form) {
-        User newUser = new User();
-        newUser.setEmail(form.getEmail());
-        newUser.setPassword(form.getPassword());
-        usersRepository.save(newUser);
-    }
-}
+//package ru.itis.springlectionsdemo.services;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//import ru.itis.springlectionsdemo.dto.UserForm;
+//import ru.itis.springlectionsdemo.models.User;
+//import ru.itis.springlectionsdemo.repositories.UsersRepository;
+//
+//@Component
+//public class SignUpServiceImpl implements SignUpService {
+//
+//    @Autowired
+//    private UsersRepository usersRepository;
+//    //
+//
+//    @Override
+//    public void signUp(UserForm form) {
+//        User newUser = new User();
+//        newUser.setEmail(form.getEmail());
+//        newUser.setPassword(form.getPassword());
+//        usersRepository.save(newUser);
+//    }
+//}
