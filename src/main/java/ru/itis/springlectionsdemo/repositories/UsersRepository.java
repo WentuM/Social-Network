2109ce75e2c6c7b5f0c6205f81f74c5d61b706c5
WentuM <kesand@mail.ru>
2021-03-19 23:02:05 +0300

    dddd

--- src/main/java/ru/itis/springlectionsdemo/repositories/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.springlectionsdemo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.itis.springlectionsdemo.models.User;
-
-
-public interface UsersRepository extends JpaRepository<User, Long> {
-
-}
