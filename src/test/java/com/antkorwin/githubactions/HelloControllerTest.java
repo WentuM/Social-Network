54e727f74e53b07c5c6d91b71151d6ea52bf4592
WentuM <kesand@mail.ru>
2021-03-19 23:08:35 +0300

    dddd2

--- src/test/java/com/antkorwin/githubactions/HelloControllerTest.java
+++ src/test/java/com/antkorwin/githubactions/HelloControllerTest.java
@@ -22,6 +22,6 @@ class HelloControllerTest {
 		             .expectStatus()
 		             .isOk()
 		             .expectBody(String.class)
-		             .isEqualTo("Hello World!");
+		             .isEqualTo("Hellqorld!");
 	}
 }
