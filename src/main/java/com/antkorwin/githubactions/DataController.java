0e92660b127d4692763c62084a95e5ecef75cd1f
WentuM <kesand@mail.ru>
2021-03-27 01:36:23 +0300

    init

---
+++ src/main/java/com/antkorwin/githubactions/DataController.java
@@ -0,0 +1,15 @@
+//package com.antkorwin.githubactions;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.Date;
+//
+//@RestController
+//public class DataController {
+//
+//    @GetMapping("/date")
+//    public String date() {
+//        return new Date().toString();
+//    }
+//}
