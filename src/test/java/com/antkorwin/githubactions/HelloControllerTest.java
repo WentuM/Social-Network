bcd171c11c6f19095c8ac9ea8dfb5a97249143ad
WentuM <kesand@mail.ru>
2021-03-27 01:09:54 +0300

    commit_shamil

--- src/test/java/com/antkorwin/githubactions/HelloControllerTest.java
+++ src/test/java/com/antkorwin/githubactions/HelloControllerTest.java
@@ -22,6 +22,6 @@ class HelloControllerTest {
 		             .expectStatus()
 		             .isOk()
 		             .expectBody(String.class)
-		             .isEqualTo("Hellqorld!");
+		             .isEqualTo("Helloworld!");
 	}
 }
