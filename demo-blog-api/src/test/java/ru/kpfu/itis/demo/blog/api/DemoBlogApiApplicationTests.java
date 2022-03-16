d10b085714319fd6cdaffc1184b8480fee039d97
danil <povarnicin@technokratos.com>
2022-03-16 19:46:53 +0300

    tests

--- demo-blog-api/src/test/java/ru/kpfu/itis/demo/blog/api/DemoBlogApiApplicationTests.java
+++ demo-blog-api/src/test/java/ru/kpfu/itis/demo/blog/api/DemoBlogApiApplicationTests.java
@@ -2,12 +2,23 @@ package ru.kpfu.itis.demo.blog.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Assert;
 
 @SpringBootTest
 class DemoBlogApiApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testSuccess() {
+        Assert.assertEquals(1, 1);
+    }
+
+    @Test
+    public void testFailure() {
+        Assert.assertEquals(1, 2);
     }
 
 }
